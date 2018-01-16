7566a7dd445d04a8881309a7a18f43c518779ea6
spvcx <spvcxxa@gmail.com>
2018-01-16 21:45:19 +0300

    return user_id fix

--- classes/User.php
+++ classes/User.php
@@ -11,7 +11,7 @@ class User {
 
         if(isset($_COOKIE['CID'])) {
             if(DB::query('SELECT user_id FROM cookie_token WHERE token=:token',array(':token'=>sha1($_COOKIE['CID'])))) {
-                $user_id = DB::query('SELECT user_id FROM cookie_token WHERE token=:token',array(':token'=>sha1($_COOKIE['CID'])));
+                $user_id = DB::query('SELECT user_id FROM cookie_token WHERE token=:token',array(':token'=>sha1($_COOKIE['CID'])))[0]['user_id'];
                 return $user_id;
             }
         } else {
